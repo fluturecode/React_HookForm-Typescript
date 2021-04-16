9ffaf284c3ee7259d43b1ac1b267b6bd20638cd2
Elizabeth Eidelson <fluturecode@gmail.com>
2021-04-16 10:37:44 -0400

    added styling to login.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
 import React from "react"
 import Login from "./components/Login"
+import styled from "styled-components"
 
-function App() {
+const LoginContainer = styled.div`
+	display: flex;
+	margin: 100px 100px 100px 100px;
+	border: 2px solid green;
+`
+const LoginInnerContainer = styled.div`
+	padding: 100px 100px 100px 100px;
+`
+function App(): JSX.Element {
 	return (
-		<>
-			<Login />
-		</>
+		<LoginContainer>
+			<LoginInnerContainer>
+				<Login />
+			</LoginInnerContainer>
+		</LoginContainer>
 	)
 }
 
--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,6 +1,14 @@
 import React from "react"
 import { useForm } from "../hooks/useForm"
+import styled from "styled-components"
 
+const LoginFormStyled = styled.div`
+	display: flex;
+`
+
+const LoginFormFields = styled.div`
+	flex-direction: column;
+`
 function Login() {
 	const initialState = {
 		email: "",
@@ -17,28 +25,29 @@ function Login() {
 	}
 
 	return (
-		<form onSubmit={onSubmit}>
-			<div>
-				<input
-					name='email'
-					id='email'
-					type='email'
-					placeholder='Email'
-					onChange={onChange}
-					required
-				/>
-
-				<input
-					name='password'
-					id='password'
-					type='password'
-					placeholder='Password'
-					onChange={onChange}
-					required
-				/>
-				<button type='submit'>Login</button>
-			</div>
-		</form>
+		<LoginFormStyled>
+			<form onSubmit={onSubmit}>
+				<LoginFormFields>
+					<input
+						name='email'
+						id='email'
+						type='email'
+						placeholder='Email'
+						onChange={onChange}
+						required
+					/>
+					<input
+						name='password'
+						id='password'
+						type='password'
+						placeholder='Password'
+						onChange={onChange}
+						required
+					/>
+					<button type='submit'>Login</button>
+				</LoginFormFields>
+			</form>
+		</LoginFormStyled>
 	)
 }
 
